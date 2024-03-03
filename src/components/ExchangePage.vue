17a555611b4a1f337583942a1d6d57a168f58a50
Dmitry <poznavshii@gmail.com>
2024-03-03 16:13:15 +0400

    fix addBalanceRUB func

--- src/components/ExchangePage.vue
+++ src/components/ExchangePage.vue
@@ -271,11 +271,12 @@ export default {
         addBalanceRUB() {
             if (Number.isInteger(Number(this.addRUB[0]))) {
                 this.balanceRUB = Number(this.balanceRUB) + Number(this.addRUB);
+                this.balanceRUB = this.balanceRUB.toFixed(2);
                 this.addRUB = '';
-                localStorage.setItem('balanceRUB', this.balanceRUB.toFixed(2));
+                localStorage.setItem('balanceRUB', this.balanceRUB);
                 this.isActive = !this.isActive;
             } else {
-                this.addRUB = '';
+                this.addRUB = ''
             }
         },
 
